e0b88a98144c8e5045aab6eaebddf2ecf874dbb7
Aнастасия <kisnastya2004@mail.ru>
2026-03-20 13:31:45 +0500

    landing load

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Карусель блока "Что такое ДИТ"
     const marquee = document.querySelector('.marquee__inner');
     if (marquee) {
         // Клонируем содержимое
         marquee.innerHTML += marquee.innerHTML;
     }
+
+    // Блок "Часто задаваемые вопросы"
+    const faqItem = document.querySelectorAll('.faq__item'),
+        btnFaqItem = document.querySelectorAll('.js-faq-trigger');
+
+    btnFaqItem.forEach((item, key) => {
+        item.addEventListener('click', () => {
+            let questionWrap = faqItem[key];
+
+            if (questionWrap) {
+                if (questionWrap.classList.contains('active')) {
+                    questionWrap.classList.remove('active');
+                } else {
+                    questionWrap.classList.add('active');
+                }
+            }
+        });
+    });
 });
